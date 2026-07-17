c6f9178abeebee9087bce3dd990955919276d61d
replic005 <riplikbade@gmail.com>
2026-07-17 07:54:10 +0545

    basic template

--- products/views.py
+++ products/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import product,category
 
 def products(request):
-    return render(request, 'products/products.html')
+    all_products = product.objects.all()
+    return render(request, 'basic/products.html', {'products': all_products})
 
 def product_detail(request,id):
     get_product = get_object_or_404(product, id=id)
-    return render(request, 'products/details.html', {'get_product': get_product})
+    return render(request, 'basic/products/details.html', {'get_product': get_product})
