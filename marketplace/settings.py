2bd02adbbdb240f2c881372be4c5637aadb7f63a
replic005 <riplikbade@gmail.com>
2026-07-29 08:01:21 +0545

    sitesetting template in settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -67,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pages.context_processors.pages_links',
+                'sitesetting.context_processors.site_settings'
             ],
         },
     },
