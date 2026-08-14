a99a3d14582096d4da61c4f4dc99e41fa3d188c6
replic005 <riplikbade@gmail.com>
2026-08-14 07:30:13 +0545

    add new pages in dashboard

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -29,7 +29,6 @@ urlpatterns = [
     path('accounts/register/', views.user_register, name='user_register'),
     path('accounts/logout/', views.user_logout, name='user_logout'),
 
-    path('accounts/activate/<uidb64>/<token>/', views.account_activate, name="account_activate"),
 
     path('accounts/dashboard/', views.user_dashboard, name='user_dashboard'),
     path('accounts/edit-profile/', views.edit_profile, name='edit_profile'),
@@ -44,13 +43,11 @@ urlpatterns = [
     path('accounts/my-sales/<int:order_product_id>/status/', views.update_delivery_status, name='seller_update_delivery_status'),
 
     path('accounts/forgot-password/', views.forgot_password, name='forgot_password'),
-    path('accounts/forgot-password-validate/<uidb64>/<token>/', views.forgot_password_validate, name='forgot_password_validate'),
 
     path('accounts/reset-password/', views.reset_password, name='reset_password'),
 
     path('accounts/change-password/', views.change_password, name='change_password'),
     path('accounts/my-requests-sent/', views.my_requests_sent, name='my_requests_sent'),
     path("accounts/my-requests-received/", views.my_requests_received, name="my_requests_received"),
-    path("accounts/fulfill/<int:pk>/", views.mark_request_fulfilled, name="mark_request_fulfilled"),
-    path("accounts/reopen/<int:pk>/", views.reopen_request, name="reopen_request"),
+
 ]
