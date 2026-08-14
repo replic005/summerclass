e1295e563412a36e16a417d2e40f4e350e76817c
replic005 <riplikbade@gmail.com>
2026-08-14 07:09:13 +0545

    manage the urls

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -15,16 +15,42 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('products/', include('products.urls')),
     path('blog/', include('blog.urls')),
-    path('pages/', include('pages.urls')),
 
-]+ static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # --- accounts: managed directly here, no separate accounts app ---
+    path('accounts/login/', views.user_login, name='user_login'),
+    path('accounts/register/', views.user_register, name='user_register'),
+    path('accounts/logout/', views.user_logout, name='user_logout'),
+
+    path('accounts/activate/<uidb64>/<token>/', views.account_activate, name="account_activate"),
+
+    path('accounts/dashboard/', views.user_dashboard, name='user_dashboard'),
+    path('accounts/edit-profile/', views.edit_profile, name='edit_profile'),
+    path("accounts/my-products/", views.my_products, name="my_products"),
+    path("accounts/product/<int:product_id>/edit/", views.edit_product, name="edit_product"),
+    # path("accounts/product/<int:product_id>/delete/", views.delete_product, name="delete_product"),
+    path("accounts/add-product/", views.add_product, name="add_product"),
+
+    path('accounts/my-orders/', views.my_orders, name='my_orders'),
+    path('accounts/order-detail/<int:order_id>/', views.order_detail, name="order_detail"),
+    path('accounts/my-sales/', views.my_sales, name='my_sales'),
+    path('accounts/my-sales/<int:order_product_id>/status/', views.update_delivery_status, name='seller_update_delivery_status'),
+
+    path('accounts/forgot-password/', views.forgot_password, name='forgot_password'),
+    path('accounts/forgot-password-validate/<uidb64>/<token>/', views.forgot_password_validate, name='forgot_password_validate'),
+
+    path('accounts/reset-password/', views.reset_password, name='reset_password'),
+
+    path('accounts/change-password/', views.change_password, name='change_password'),
+    path('accounts/my-requests-sent/', views.my_requests_sent, name='my_requests_sent'),
+    path("accounts/my-requests-received/", views.my_requests_received, name="my_requests_received"),
+    path("accounts/fulfill/<int:pk>/", views.mark_request_fulfilled, name="mark_request_fulfilled"),
+    path("accounts/reopen/<int:pk>/", views.reopen_request, name="reopen_request"),
+]
